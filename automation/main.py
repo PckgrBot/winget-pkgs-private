4ce3ee39da577492cfe31e020bb47970cc6cbd7f
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-02-21 12:50:05 +0000

    Update main.py

--- automation/main.py
+++ automation/main.py
@@ -320,6 +320,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Wingetcreate, list_to_str(Urls), Version, id, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+# Add Notepad++.Notepad++_Pckgr to Update List
+    id = "Notepad++.Notepad++_Pckgr"
+    JSON = requests.get("https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe") and not("sig" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Wingetcreate, list_to_str(Urls), str_pop(Version, 0), id, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
